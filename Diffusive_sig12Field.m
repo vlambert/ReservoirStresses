016677108fd7f189c8727825d0744f7b52e373c9
Valere Lambert <valerelambert@gmail.com>
2021-01-14 11:55:57 -0800

    Update Diffusive_sig12Field.m

--- Diffusive_sig12Field.m
+++ Diffusive_sig12Field.m
@@ -1,7 +1,7 @@
 %% % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 %                  Shear stress, Sigma_12 / mu             %  
 %         at time t for reservoir with diffusivity c       %
-%                  (Lambert and Tsai, 2019)                %  
+%                  (Lambert and Tsai, 2020)                %  
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %%
 %                     Free Surface
 %   <----------------------------------------------------> x1

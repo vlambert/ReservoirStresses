df79b650e72832bdd7a7a7a9b55c157f7bbd8c79
Valere Lambert <valerelambert@gmail.com>
2021-01-14 11:56:27 -0800

    Update Diffusive_u1.m

--- Diffusive_u1.m
+++ Diffusive_u1.m
@@ -1,7 +1,7 @@
 %% % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 %            Horizontal surface displacements, u1          %  
 %         at time t for reservoir with diffusivity c       %
-%                  (Lambert and Tsai, 2019)                %  
+%                  (Lambert and Tsai, 2020)                %  
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %%
 %                     Free Surface
 %   <----------------------------------------------------> x1
